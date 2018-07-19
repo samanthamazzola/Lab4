78246cfa1ab769e05472049a0325d62ea2f897be
samantha mazzola <smazzola7@gmail.com>
2018-07-19 16:12:46 -0400

    adding Lab 4 code 	new file:   .vs/Lab4/xs/UserPrefs.xml 	new file:   .vs/Lab4/xs/sqlite3/db.lock 	new file:   .vs/Lab4/xs/sqlite3/storage.ide 	new file:   .vs/Lab4/xs/sqlite3/storage.ide-shm 	new file:   .vs/Lab4/xs/sqlite3/storage.ide-wal 	new file:   Lab4.sln 	new file:   Lab4/Lab4.csproj 	new file:   Lab4/Lab4.csproj.user 	new file:   Lab4/Program.cs 	new file:   Lab4/Properties/AssemblyInfo.cs 	new file:   Lab4/bin/Debug/Lab4.exe

---
+++ Lab4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true) 
+            {
+                double Square, Cube; //declare variables
+
+                //prompt
+                Console.WriteLine("Learn your squares and cubes!");
+                //input 
+                Console.Write("Enter an integer: ");
+                //convert string to double
+                int userInput = int.Parse(Console.ReadLine());
+                //print tab headers. added a space to center
+                Console.WriteLine("\t Number\t Square\t Cube");
+                //adds the line separation
+                Console.WriteLine("\t=======\t=======\t=======");  
+                //loop as many times as the userIput/counter value, then calculate for (square and cuded) and display values
+                for (int i = 1; i <= userInput; i++) 
+                {
+                    Square = i * i;
+                    Cube = i * i * i;
+                    Console.WriteLine("\t{0}\t{1}\t{2}", i, Square, Cube);
+                    //string header = string.Format("{0, -10}", "number");
+                }
+
+                //validation 
+                Console.Write("Would you like to continue? (y/n): "); 
+
+                string answer = Console.ReadLine().ToLower();//converts input to lowercase  
+                if (answer != "y") 
+                {
+                    break;                
+                }
+            }
+        }
+    }
+
+}
+
+
+//https://www.dreamincode.net/forums/topic/16046-calculating-square-and-cubes/
+
+/* Square = Number * Number;
+Cube = Square * Number;
+Console.WriteLine("\t{0}\t{1}\t{2}", Number, Square, Cube);*/
+
+
+// end of calculate and display 
